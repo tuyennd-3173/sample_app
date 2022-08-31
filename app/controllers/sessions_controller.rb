df6b2967c69861fea67fd9b1893189fcaa1f95f5
nguyen.dinh.tuyen <SUN-ASTERISK\nguyen.dinh.tuyen@b123173-pc.sun-asterisk.com>
2022-08-31 14:15:22 +0700

    Chapter 8

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by email: params[:session][:email].downcase
+    if user&.authenticate params[:session][:password]
+      log_in user
+      redirect_to login_url
+    else
+      flash.now[:danger] = I18n.t(".invalid_user")
+      render :new
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
+end
