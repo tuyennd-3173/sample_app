15040f0ca6585aa25d539cea69614879115294da
nguyen.dinh.tuyen <SUN-ASTERISK\nguyen.dinh.tuyen@b123173-pc.sun-asterisk.com>
2022-09-07 10:22:10 +0700

    Chapter 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   before_save :downcase_email
+  attr_accessor :remember_token
 
   validates :name, presence: true, length: {maximum: Settings.length.digit_10}
   validates :email, presence: true,
@@ -11,6 +12,36 @@ class User < ApplicationRecord
   validates :password, presence: true,
     length: {minimum: Settings.length.digit_6}, if: :password
   has_secure_password
+
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_column :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false unless remember_token
+
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_column :remember_digest, nil
+  end
 
   private
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_05_065612) do
-  
+ActiveRecord::Schema.define(version: 2022_09_06_101151) do
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
+
 end
